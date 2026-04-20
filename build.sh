92ae957aea94b35340e4ed0db65bfb7f5022bb3d
Your Name <you@example.com>
2026-04-20 09:25:40 +0900

    mod

--- build.sh
+++ build.sh
@@ -7,4 +7,9 @@ echo $DUMPFILE
 python mkjisyo.py $DUMPFILE
 
 # 重複行削除
-awk '!a[$0]++' jisyo.txt > out.txt
+awk '!a[$0]++' jisyo.txt | sort > mozc.txt
+
+# SKK用辞書の作成
+cat skk_header.txt > skk.txt
+awk '{print $1 " /" $2 "/"}' mozc.txt | sort >> skk.txt
+
