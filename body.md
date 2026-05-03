983df0664f270d852cf5fc86b72c5481ebb77b83
Your Name <you@example.com>
2026-05-04 08:06:43 +0900

    mod

--- body.md
+++ body.md
@@ -1,1 +1,12 @@
 release
+Wikipedia人名辞書です。毎月一度自動生成しています。
+１，２文字の物を含めると日本語変換に支障をきたす（※１）ため、１，２文字除外版も作っています。
+お好きな方をご利用ください。
+（※１）例えば、「とき」の変換時にずらずら「朱鷺 朱鷺 杜夔 杜琦 杜畿 杜祁 杜祺 杜驥」などがリストに出てくるなど。
+
+mozc_all.txt: mozc用全て入り辞書
+mozc_over3.txt: mozc用１，２文字除外辞書
+skk_all.txt: SKK用全て入り辞書
+skk_over3.txt: SKK用１，２文字除外辞書
+msime_all.txt: MSIME用全て入り辞書
+msime_over3.txt: MSIME用１，２文字除外辞書
--- build.sh
+++ build.sh
@@ -3,33 +3,53 @@
 DUMPFILE="$1"
 echo $DUMPFILE
 
-# 辞書作成
-python mkjisyo.py $DUMPFILE
-
-# 1,2文字の読みは削除する
-# →日本語漢字変換のご動作を防ぐため
-grep -v -P "^.{1,2}\t" jisyo.txt >jisyo2.txt
-
-# 重複行削除
-awk '!a[$0]++' jisyo2.txt >nodup.txt
-
 # 作成日
 DATE=$(date)
 
 # sortをひらがな、カタカナ、漢字順にする
 export LC_COLLATE=C
 
-# mozc用辞書の作成
-cat mozc_header.txt >mozc.txt
-echo "# Created: $DATE" >>mozc.txt
-cat nodup.txt | sort >>mozc.txt
+function create_jisyo() {
+    BASE=$1
+    TYPE=$2
 
-# SKK用辞書の作成
-cat skk_header.txt >skk.txt
-echo ";; Created: $DATE" >>skk.txt
-awk '{print $1 " /" $2 "/"}' nodup.txt | sort >>skk.txt
+    MOZC=mozc_${TYPE}.txt
 
-# MS-IME辞書の作成
-cat msime_header.txt >msime.txt
-echo "! Created: $DATE" >>msime.txt
-cat nodup.txt | sort >>msime.txt
+    # mozc用辞書の作成
+    cat mozc_header.txt >$MOZC
+    echo "# Created: $DATE" >>$MOZC
+    cat $BASE | sort >>$MOZC
+
+    SKK=skk_${TYPE}.txt
+
+    # SKK用辞書の作成
+    cat skk_header.txt >$SKK
+    echo ";; Created: $DATE" >>$SKK
+    awk '{print $1 " /" $2 "/"}' $BASE | sort >>$SKK
+
+    MSIME=msime_${TYPE}.txt
+
+    # MS-IME辞書の作成
+    cat msime_header.txt >$MSIME
+    echo "! Created: $DATE" >>$MSIME
+    cat $BASE | sort >>$MSIME
+}
+
+function main() {
+    # 辞書作成
+    python mkjisyo.py $DUMPFILE
+
+    # 重複行削除
+    awk '!a[$0]++' jisyo.txt >nodup_all.txt
+
+    # 1,2文字の読みは削除する
+    # →日本語漢字変換の誤動作を防ぐため
+    grep -v -P "^.{1,2}\t" nodup_all.txt >nodup_over3.txt
+
+    create_jisyo nodup_all.txt all
+    create_jisyo nodup_over3.txt over3
+}
+
+main
+
+# EOF
