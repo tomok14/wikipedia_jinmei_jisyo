5468a9a50dead5c9b4835184d336ce52c85c3f14
Your Name <you@example.com>
2026-06-06 05:41:05 +0900

    mod

--- bin/build.sh
+++ bin/build.sh
@@ -11,7 +11,7 @@ echo "DUMPFILE=$DUMPFILE"
 # 作成日
 DATE=$(TZ=Asia/Tokyo date "+%Y/%-m/%-d %H:%M:%S JST")
 #DATE=$(LANG=C TZ=-9 date '+%a %b %d %H:%M:%S JST %Y')
-echo $DATE
+echo "$DATE"
 
 # sortをひらがな、カタカナ、漢字順にする
 export LC_COLLATE=C
@@ -25,16 +25,16 @@ function create_jisyo_core() {
 
     echo "COMT=$COMT"
 
-    echo "${CC} ${DICNAME}用Wikipedia人名辞書: $COMT" >$DICFILE
-    echo "${CC} 生成元のデータ: $DUMPFILE" >>$DICFILE
-    echo "${CC} 有効項目数: $LINE" >>$DICFILE
-    echo "${CC} 読み, 語句, 品詞" >>$DICFILE
-    echo "${CC} Created: $DATE" >>$DICFILE
-    echo "" >>$DICFILE
+    echo "${CC} ${DICNAME}用Wikipedia人名辞書: $COMT" >"$DICFILE"
+    echo "${CC} 生成元のデータ: $DUMPFILE" >>"$DICFILE"
+    echo "${CC} 有効項目数: $LINE" >>"$DICFILE"
+    echo "${CC} Created: $DATE" >>"$DICFILE"
+    echo "${CC} 読み, 語句, 品詞" >>"$DICFILE"
+    echo "" >>"$DICFILE"
     if [ "$DICNAME" = "SKK" ]; then
-        awk '{print $1 " /" $2 "/"}' $BASE | sort >>$DICFILE
+        awk '{print $1 " /" $2 "/"}' "$BASE" | sort >>"$DICFILE"
     else
-        cat $BASE | sort >>$DICFILE
+        cat "$BASE" | sort >>"$DICFILE"
     fi
 }
 
@@ -43,24 +43,24 @@ function create_jisyo() {
     TYPE=$2
     COMT="$3"
 
-    LINE=$(wc -l $BASE | cut -f 1 -d ' ')
+    LINE=$(wc -l "$BASE" | cut -f 1 -d ' ')
 
     # mozc用辞書の作成
     MOZC=$OUTDIR/mozc_${TYPE}.txt
-    create_jisyo_core "mozc" $MOZC "#" $BASE $DUMPFILE
+    create_jisyo_core "mozc" "$MOZC" "#" "$BASE" "$DUMPFILE"
 
     # SKK用辞書の作成
     SKK=$OUTDIR/skk_${TYPE}.txt
-    create_jisyo_core "SKK" $SKK ";;" $BASE $DUMPFILE
+    create_jisyo_core "SKK" "$SKK" ";;" "$BASE" "$DUMPFILE"
 
     # MS-IME辞書の作成
     MSIME=$OUTDIR/msime_${TYPE}.txt
-    create_jisyo_core "MS-IME" $MSIME "!" $BASE $DUMPFILE
+    create_jisyo_core "MS-IME" "$MSIME" "!" "$BASE" "$DUMPFILE"
 }
 
 function main() {
     # 辞書作成
-    python src/mkjisyo.py $DUMPFILE >$OUTDIR/jisyo.txt
+    python src/mkjisyo.py "$DUMPFILE" >$OUTDIR/jisyo.txt
 
     # 空行削除「
     grep -v "^$" $OUTDIR/jisyo.txt >$OUTDIR/jisyo2.txt
