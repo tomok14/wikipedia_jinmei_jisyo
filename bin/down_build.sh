a7ba393cda40235eb87fb5b889d7203a15dc7292
Your Name <you@example.com>
2026-06-27 05:47:32 +0900

    mod

--- bin/down_build.sh
+++ bin/down_build.sh
@@ -1,4 +1,9 @@
+#!/usr/bin/bash
+
 ./bin/downarc_legacy.sh -a
-FILE=$(ls jawiki*.bz2 | tail -1)
-echo "$FILE"
-./bin/build.sh "$FILE"
+#FILE=$(ls jawiki*.bz2 | tail -1)
+shopt -s nullglob
+files=(jawiki*.bz2)
+file=${files[-1]}
+echo "$file"
+./bin/build.sh "$file"
